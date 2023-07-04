36746c7ba742dbb194af5687d216fe1b3b99f24e
meinci <meinci@hotmail.de>
2023-07-04 13:00:17 +0200

    commit von 4.7.23

--- app/src/main/java/com/mehmet_inci/firebasetest/FeedActivity.java
+++ app/src/main/java/com/mehmet_inci/firebasetest/FeedActivity.java
@@ -1,14 +1,45 @@
 package com.mehmet_inci.firebasetest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FeedActivity extends AppCompatActivity {
+	private FirebaseAuth auth;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_feed);
+
+		auth = FirebaseAuth.getInstance();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.option_menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		if (item.getItemId() == R.id.add_post){
+			//Upload Activity
+		} else if (item.getItemId() == R.id.signout) {
+			//Signout
+			auth.signOut();
+			Intent intentToSignUp = new Intent(FeedActivity.this, SignUpActivity.class);
+			startActivity(intentToSignUp);
+			finish();
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
--- app/src/main/java/com/mehmet_inci/firebasetest/SignUpActivity.java
+++ app/src/main/java/com/mehmet_inci/firebasetest/SignUpActivity.java
@@ -12,7 +12,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.mehmet_inci.firebasetest.databinding.ActivityFeedBinding;
+import com.google.firebase.auth.FirebaseUser;
 import com.mehmet_inci.firebasetest.databinding.ActivitySignupBinding;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -28,28 +28,39 @@ public class SignUpActivity extends AppCompatActivity {
 
 		auth = FirebaseAuth.getInstance();
 
+		FirebaseUser user = auth.getCurrentUser();
+		if (user != null){
+			Intent intent = new Intent(SignUpActivity.this, FeedActivity.class);
+			startActivity(intent);
+			finish();
+		}
+
 	}
-	public void signup_button(View view){
+	public void signUpClicked(View view){
 		String email = binding.emailText.getText().toString();
 		String password = binding.passwordText.getText().toString();
-		auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-			@Override
-			public void onSuccess(AuthResult authResult) {
+		if (email.equals("") || password.equals("")) {
+			   Toast.makeText(SignUpActivity.this, "Enter Email and Password", Toast.LENGTH_LONG).show();
+		} else {
+			auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(authResult -> {
 				Intent intent = new Intent(SignUpActivity.this, FeedActivity.class);
 				startActivity(intent);
 				finish();
-			}
-		}).addOnFailureListener(new OnFailureListener() {
-			@Override
-			public void onFailure(@NonNull Exception e) {
-				Toast.makeText(SignUpActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-			}
-		});
-
-
+			}) .addOnFailureListener(e -> Toast.makeText(SignUpActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show());
+		}
 
 	}
-	public void signin_button(View view){
-
+	public void signInClicked(View view){
+  			String email = binding.emailText.getText().toString();      
+	        String password = binding.passwordText.getText().toString();
+			if (email.equals("")|| password.equals("")) {
+				Toast.makeText(SignUpActivity.this, "Enter Email and Password", Toast.LENGTH_LONG).show();
+			}  else {
+				auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(authResult -> {
+		  			Intent intent = new Intent(SignUpActivity.this, FeedActivity.class);
+					startActivity(intent);
+					finish();
+				}).addOnFailureListener(e ->Toast.makeText(SignUpActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show());
+			}
 	}
 }
